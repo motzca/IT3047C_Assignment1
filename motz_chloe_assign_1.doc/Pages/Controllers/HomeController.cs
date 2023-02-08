5b55c954608b407f9fba411a72b2fe815071d21d
finni <finni@LAPTOP-IQTB0MRC>
2023-02-07 22:09:18 -0500

    Add project files.

---
+++ motz_chloe_assign_1.doc/Pages/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using motz_chloe_assign_1.doc.Pages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace motz_chloe_assign_1.doc.Pages.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+    }
+}
---
+++ motz_chloe_assign_1.doc/Pages/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace motz_chloe_assign_1.doc.Pages.Models
+{
+    public class Genre 
+    {
+        public string GenreId { get; set; }
+        public string Name { get; set; }
+    }
+}
